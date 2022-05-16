bc839bf3c985bcf762f4f7e4bdf67cb1045ecec5
Rehan Raza <rhn.raza121@gmail.com>
2022-05-16 22:28:25 +0530

    update UI

--- src/routers/dashboard.js
+++ src/routers/dashboard.js
@@ -16,4 +16,11 @@ router.get('/archive', auth, (req, res) => {
 router.get('/master_list', auth, (req, res) => {
   res.render('master_list.ejs', { user: req.user })
 })
+router.get('/admin/addOffer', (req, res) => {
+  res.render('Add_offer.ejs')
+})
+router.get('/admin/removeOffer', (req, res) => {
+  res.render('remove_offer.ejs')
+})
+
 module.exports = router
--- src/routers/user.js
+++ src/routers/user.js
@@ -14,18 +14,10 @@ router.get('/users/signup', (req, res) => {
   res.render('register.ejs')
 })
 
+// UI Remove User
 router.get('/users/remove', (req, res) => {
   res.render('remove_user.ejs')
 })
-
-router.get('/users/addOffer', (req, res) => {
-  res.render('Add_offer.ejs')
-})
-
-router.get('/users/removeOffer', (req, res) => {
-  res.render('remove_offer.ejs')
-})
-
 
 // signup
 router.post('/users', async (req, res) => {
