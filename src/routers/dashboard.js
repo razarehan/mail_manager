c9ce17589f6ae1d9ee13deeb2cc08f3e15b9c4d5
Rehan Raza <rhn.raza121@gmail.com>
2022-05-16 18:17:46 +0530

    sending user object from BE to FE

--- src/routers/dashboard.js
+++ src/routers/dashboard.js
@@ -8,12 +8,12 @@ router.get('/dashboard', auth, (req, res) => {
   res.render('dashboard.ejs', { user: req.user })
 })
 router.get('/jobs_today', auth, (req, res) => {
-  res.render('jobs_today.ejs')
+  res.render('jobs_today.ejs', { user: req.user })
 })
 router.get('/archive', auth, (req, res) => {
-  res.render('archive.ejs')
+  res.render('archive.ejs', { user: req.user })
 })
 router.get('/master_list', auth, (req, res) => {
-  res.render('master_list.ejs')
+  res.render('master_list.ejs', { user: req.user })
 })
 module.exports = router
